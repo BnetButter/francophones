351a41c7c05ef31b3bafa55bbff147f2332c62f4
Kevin <kevin@e-itheta.com>
2021-10-21 17:51:43 +0000

    Temperature styling

--- src/components/weather/temp.tsx
+++ src/components/weather/temp.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import WeatherData from "./WeatherData"
 import * as tempscale from "./TempScale"
+import "./temp.css"
 
 export interface TemperatureI {
     data: WeatherData;
@@ -21,13 +22,13 @@ function Temperature(props: TemperatureI)
                 {temp}
             </div>
             <div>
-                <div>
+                <div className="Subtemp">
                     <div className="label">Low</div>
-                    <div className="subtemp">{low}</div>
+                    <div>{low}</div>
                 </div>
-                <div>
+                <div className="Subtemp">
                     <div className="label">High</div>
-                    <div className="subtemp">{high}</div>
+                    <div>{high}</div>
                 </div>
             </div>
         </div>
